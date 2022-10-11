381b0698cf2a6c7583d0a7d2087252cfc6159fbb
Jim McAndrew <jim@loc8.us>
2022-10-11 16:01:20 -0600

    Add WMS plugin

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,116 +1,79 @@
-import { default as MapLibrary, VectorSourceSpecification, Evented, Dispatcher } from 'maplibre-gl';
-export declare type CartoSourceSpecification = Omit<VectorSourceSpecification, 'type' | 'tiles' | 'url' | 'scheme'> & {
-    'type': 'carto';
-    'user': string;
-    'table'?: string;
-    'sql'?: string;
-    'server'?: string;
+import { default as MapLibrary, Evented, Dispatcher, RasterSourceSpecification } from 'maplibre-gl';
+export declare type WmsSourceSpecification = Omit<RasterSourceSpecification, 'type' | 'tiles' | 'url' | 'scheme'> & {
+    'url': string;
+    'layers': Array<string | number>;
+    'transparent'?: boolean;
+    tileSize?: number;
 };
-export declare type CartoApiLayerOptions = {
-    /** The SQL request to the user database that will fetch the rendered data.
-     * The SQL request should include the following Mapnik layer configurations:
-     *   geom_column
-     *   interactivity
-     *   attributes
-     *
-     *   Note: The SQL request may contain substitutions tokens, such as !bbox!, !pixel_width! and !pixel_height!.
-     *   It is suggested to define the layergroup minzoom and extent variables to prevent errors.
-     *
-     */
-    sql: string;
-    /** A string value, specifying the CartoCSS style version of the CartoCSS attribute. */
-    cartocss_version?: string;
-    /** The name of the column containing the geometry. Default "the_geom_webmercator" */
-    geom_column?: string;
-    /** Defines the type of column as either geometry or raster. Default "geometry" */
-    geom_type?: "geometry" | "raster";
-    /**
-     * Defines the raster band (this option is only applicable when the geom_type=raster.)
-     *
-     * Note: If the default, or no value is specified, raster bands are interpreted as either:
-     *
-     * grayscale (for single bands)
-     * RGB (for 3 bands)
-     * RGBA (for 4 bands).
-     */
-    raster_band?: string;
-    /** The spatial reference identifier for the geometry column. Default "3857" */
-    srid: string;
-    /** A string of values containing the tables that the Mapnik layer SQL configuration is using. This value is used if there is a problem guessing what the affected tables are from the SQL configuration (i.e. when using PL/SQL functions). */
-    affected_tables?: string;
-    /** A string of values that contains the fields rendered inside grid.json. All the parameters should be exposed as a result of executing the Mapnik layer SQL query. */
-    interactivity?: string;
-    /** The id and column values returned by the Mapnik attributes service.
-     * (This option is disabled if no configuration is defined).
-     * You must specify this value as part of the Mapnik layer SQL configuration.
-   */
-    attributes?: [
-        {
-            'id': string;
-            'columms': string;
-        }
-    ];
+export declare type WmsApiLayerOptions = {
+    /**   Service name. Value is WMS. */
+    service: 'WMS';
+    /** Service version. Value is one of 1.0.0, 1.1.0, 1.1.1, 1.3.0. */
+    version: '1.0.0' | '1.1.0' | '1.1.1';
+    /**   Operation name. Value is GetMap. */
+    request: 'GetMap';
+    /**   Layers to display on map.Value is a comma - separated list of layer names. */
+    layers: string;
+    /** Styles in which layers are to be rendered.Value is a comma - separated list of style names,
+     * or empty if default styling is required.Style names may be empty in the list,
+     * to use default layer styling. */
+    styles: string;
+    /** Spatial Reference System for map output.Value is in form EPSG: nnn. */
+    srs: string;
+    /**   Bounding box for map extent.Value is minx, miny, maxx, maxy in units of the SRS. */
+    bbox: string;
+    /** Width of map output, in pixels. */
+    width: string;
+    /** Height of map output, in pixels. */
+    height: string;
+    /** Format for the map output. */
+    format: string;
+    /** Whether the map background should be transparent.Values are true or false.Default is false */
+    transparent?: boolean;
+    /**  Background color for the map image.Value is in the form RRGGBB.Default is FFFFFF(white). */
+    bgcolor?: string;
+    /** Format in which to report exceptions.Default value is application / vnd.ogc.se_xml. */
+    exceptions?: string;
+    /**   Time value or range for map data. */
+    time?: string;
+    /**   A URL referencing a StyledLayerDescriptor XML file which controls or enhances map layers and styling */
+    sld?: string;
+    /** A URL - encoded StyledLayerDescriptor XML document which controls or enhances map layers and styling */
+    sld_body?: string;
 };
-export declare type CartoApiLayerObj = {
-    /**
-      mapnik - rasterized tiles
-      cartodb - an alias for mapnik (for backward compatibility)
-      torque - render vector tiles in torque format
-      http - load tiles over HTTP
-      plain - color or background image url
-      named - use a Named Map as a layer
-    */
-    type: "mapnik" | "cartodb" | "torque" | "http" | "plain" | "named";
-    options: CartoApiLayerOptions;
+export declare type WmsApiLayerOptions130 = Omit<WmsApiLayerOptions, 'version' | 'srs'> & {
+    version: '1.3.0';
+    crs: string;
 };
-export declare type CartoApiV1CreateMap = {
-    /** Spec version to use for validation. */
-    version?: string;
-    /**   The default map extent for the map projection. Note: Currently, only webmercator is supported. */
-    extent?: string;
-    /**   The spatial reference identifier for the map. */
-    srid?: string;
-    /**   The maximum zoom level for your map. A request beyond the defined maxzoom returns a 404 error. */
-    maxzoom?: string;
-    /** The minimum zoom level for your map. A request beyond the defined minzoom returns a 404 error. */
-    minzoom?: string;
-    layers: Array<CartoApiLayerObj>;
-};
-export default function CartoSource(mapLibrary: typeof MapLibrary, options?: Partial<CartoSourceSpecification>): {
-    new (id: string, originalSource: CartoSourceSpecification, dispatcher: Dispatcher, eventedParent: Evented): {
-        _originalSource: CartoSourceSpecification;
+export default function WmsSource(mapLibrary: typeof MapLibrary): {
+    new (id: string, originalSource: WmsSourceSpecification, dispatcher: Dispatcher, eventedParent: Evented): {
+        _originalSource: WmsSourceSpecification;
         load(): void;
-        convertToSource(): Promise<Required<VectorSourceSpecification>>;
-        type: "vector";
+        convertToSource(): Promise<Required<RasterSourceSpecification>>;
+        type: "raster" | "raster-dem";
         id: string;
         minzoom: number;
         maxzoom: number;
         url: string;
         scheme: string;
         tileSize: number;
-        promoteId: import("maplibre-gl").PromoteIdSpecification;
-        _options: VectorSourceSpecification;
-        _collectResourceTiming: boolean;
+        bounds: [number, number, number, number];
+        tileBounds: import("maplibre-gl").TileBounds;
+        roundZoom: boolean;
         dispatcher: Dispatcher;
         map: import("maplibre-gl").Map;
-        bounds: [number, number, number, number];
         tiles: string[];
-        tileBounds: import("maplibre-gl").TileBounds;
-        reparseOverscaled: boolean;
-        isTileClipped: boolean;
-        _tileJSONRequest: import("maplibre-gl").Cancelable;
         _loaded: boolean;
+        _options: RasterSourceSpecification | import("maplibre-gl").RasterDEMSourceSpecification;
+        _tileJSONRequest: import("maplibre-gl").Cancelable;
         loaded(): boolean;
-        hasTile(tileID: import("maplibre-gl").OverscaledTileID): boolean;
         onAdd(map: import("maplibre-gl").Map): void;
-        setSourceProperty(callback: Function): void;
-        setTiles(tiles: string[]): any;
-        setUrl(url: string): any;
         onRemove(): void;
         serialize(): any;
+        hasTile(tileID: import("maplibre-gl").OverscaledTileID): boolean;
         loadTile(tile: import("maplibre-gl").Tile, callback: import("maplibre-gl").Callback<void>): void;
-        abortTile(tile: import("maplibre-gl").Tile): void;
-        unloadTile(tile: import("maplibre-gl").Tile): void;
+        abortTile(tile: import("maplibre-gl").Tile, callback: import("maplibre-gl").Callback<void>): void;
+        unloadTile(tile: import("maplibre-gl").Tile, callback: import("maplibre-gl").Callback<void>): void;
         hasTransition(): boolean;
         _listeners: import("maplibre-gl").Listeners;
         _oneTimeListeners: import("maplibre-gl").Listeners;
--- src/index.ts
+++ src/index.ts
@@ -1,177 +1,148 @@
 import {
   default as MapLibrary,
-  VectorSourceSpecification,
   Evented,
   Source,
   Dispatcher,
-  VectorTileSource,
+  RasterSourceSpecification,
 } from 'maplibre-gl';
 
-export type CartoSourceSpecification = Omit<VectorSourceSpecification, 'type' | 'tiles' | 'url' | 'scheme'> & {
-  'type': 'carto',
-  'user': string,
-  'table'?: string,
-  'sql'?: string,
-  'server'?: string
+export type WmsSourceSpecification = Omit<RasterSourceSpecification, 'type' | 'tiles' | 'url' | 'scheme'> & {
+  'url': string,
+  'layers': Array<string | number>,
+  'transparent'?: boolean,
+  tileSize?: number
 };
 
-// https://carto.com/developers/maps-api/reference/#operation/instantiateAnonymousMap
-export type CartoApiLayerOptions = {
-  /** The SQL request to the user database that will fetch the rendered data.
-   * The SQL request should include the following Mapnik layer configurations:
-   *   geom_column
-   *   interactivity
-   *   attributes
-   * 
-   *   Note: The SQL request may contain substitutions tokens, such as !bbox!, !pixel_width! and !pixel_height!. 
-   *   It is suggested to define the layergroup minzoom and extent variables to prevent errors.
-   *
-   */
-  sql: string,
-  /** A string value, specifying the CartoCSS style version of the CartoCSS attribute. */
-  cartocss_version?: string,
-  /** The name of the column containing the geometry. Default "the_geom_webmercator" */
-  geom_column?: string
-  /** Defines the type of column as either geometry or raster. Default "geometry" */
-  geom_type?: "geometry" | "raster",
-  /**
-   * Defines the raster band (this option is only applicable when the geom_type=raster.)
-   * 
-   * Note: If the default, or no value is specified, raster bands are interpreted as either:
-   * 
-   * grayscale (for single bands)
-   * RGB (for 3 bands)
-   * RGBA (for 4 bands).
-   */
-  raster_band?: string,
-  /** The spatial reference identifier for the geometry column. Default "3857" */
-  srid: string,
-  /** A string of values containing the tables that the Mapnik layer SQL configuration is using. This value is used if there is a problem guessing what the affected tables are from the SQL configuration (i.e. when using PL/SQL functions). */
-  affected_tables?: string,
-  /** A string of values that contains the fields rendered inside grid.json. All the parameters should be exposed as a result of executing the Mapnik layer SQL query. */
-  interactivity?: string,
-  /** The id and column values returned by the Mapnik attributes service. 
-   * (This option is disabled if no configuration is defined). 
-   * You must specify this value as part of the Mapnik layer SQL configuration.
- */
-  attributes?: [
-    {
-      'id': string,
-      'columms': string
-    }
-  ]
-};
-export type CartoApiLayerObj = {
-  /** 
-    mapnik - rasterized tiles
-    cartodb - an alias for mapnik (for backward compatibility)
-    torque - render vector tiles in torque format
-    http - load tiles over HTTP
-    plain - color or background image url
-    named - use a Named Map as a layer
-  */
-  type: "mapnik" | "cartodb" | "torque" | "http" | "plain" | "named",
-  options: CartoApiLayerOptions
+const WmsSourceDefaults = {
+  transparent: true,
+  tileSize: 256
 }
-export type CartoApiV1CreateMap = {
-  /** Spec version to use for validation. */
-  version?: string,
-  /**   The default map extent for the map projection. Note: Currently, only webmercator is supported. */
-  extent?: string,
-  /**   The spatial reference identifier for the map. */
-  srid?: string,
-  /**   The maximum zoom level for your map. A request beyond the defined maxzoom returns a 404 error. */
-  maxzoom?: string,
-  /** The minimum zoom level for your map. A request beyond the defined minzoom returns a 404 error. */
-  minzoom?: string,
-  layers: Array<CartoApiLayerObj>
+
+// https://docs.geoserver.org/stable/en/user/services/wms/reference.html
+export type WmsApiLayerOptions = {
+  /**   Service name. Value is WMS. */
+  service: 'WMS',
+  /** Service version. Value is one of 1.0.0, 1.1.0, 1.1.1, 1.3.0. */
+  version: '1.0.0' | '1.1.0' | '1.1.1',
+  /**   Operation name. Value is GetMap. */
+  request: 'GetMap',
+  /**   Layers to display on map.Value is a comma - separated list of layer names. */
+  layers: string,
+  /** Styles in which layers are to be rendered.Value is a comma - separated list of style names, 
+   * or empty if default styling is required.Style names may be empty in the list, 
+   * to use default layer styling. */
+  styles: string,
+  /** Spatial Reference System for map output.Value is in form EPSG: nnn. */
+  srs: string,
+  /**   Bounding box for map extent.Value is minx, miny, maxx, maxy in units of the SRS. */
+  bbox: string
+  /** Width of map output, in pixels. */
+  width: string,
+  /** Height of map output, in pixels. */
+  height: string,
+  /** Format for the map output. */
+  format: string,
+  /** Whether the map background should be transparent.Values are true or false.Default is false */
+  transparent?: boolean,
+  /**  Background color for the map image.Value is in the form RRGGBB.Default is FFFFFF(white). */
+  bgcolor?: string,
+  /** Format in which to report exceptions.Default value is application / vnd.ogc.se_xml. */
+  exceptions?: string,
+  /**   Time value or range for map data. */
+  time?: string,
+  /**   A URL referencing a StyledLayerDescriptor XML file which controls or enhances map layers and styling */
+  sld?: string,
+  /** A URL - encoded StyledLayerDescriptor XML document which controls or enhances map layers and styling */
+  sld_body?: string,
+};
+
+export type WmsApiLayerOptions130 = Omit<WmsApiLayerOptions, 'version' | 'srs'> & {
+  version: '1.3.0',
+  crs: string
 };
 
 
-const cartoDefaults = {
-  'type': 'carto',
-  'server': 'carto.com'
-} as CartoSourceSpecification;
 
-export default function CartoSource(mapLibrary: typeof MapLibrary, options: Partial<CartoSourceSpecification> = cartoDefaults) {
-  return class CartoSource extends mapLibrary['VectorTileSource'] implements Source {
-    _originalSource: CartoSourceSpecification;
+export default function WmsSource(mapLibrary: typeof MapLibrary) {
+  return class WmsSource extends mapLibrary['RasterTileSource'] implements Source {
+    _originalSource: WmsSourceSpecification;
 
-    constructor(id: string, originalSource: CartoSourceSpecification, dispatcher: Dispatcher, eventedParent: Evented) {
-      super(id, { 'type': 'vector', 'collectResourceTiming': true }, dispatcher, eventedParent);
+    constructor(id: string, originalSource: WmsSourceSpecification, dispatcher: Dispatcher, eventedParent: Evented) {
+      super(id, { 'type': 'raster' }, dispatcher, eventedParent);
 
       // Set the defaults
       this.id = id;
-      this._originalSource = { ...options, ...originalSource };
-
-      // If there is not a SQL query, but there's a table, make the query 'SELECT * FROM {TABLE}'
-      if (this._originalSource.table && !this._originalSource.sql) {
-        this._originalSource.sql = `SELECT * FROM "${this._originalSource.user}"."${this._originalSource.table}";`;
-      } else if (this._originalSource.sql && !this._originalSource.table) {
-        this._originalSource.table = undefined;
-      } else if (!this._originalSource.sql && !this._originalSource.table) {
-        throw new Error(`${this._originalSource.type} requires either a sql or table parameter`);
-      }
+      this._originalSource = {...WmsSourceDefaults, ...originalSource};
     }
 
     load() {
       this.convertToSource().then(convertedSource => {
+        this.type = 'raster';
         this.url = convertedSource.url; //
         this.tiles = convertedSource.tiles; //?: string[] | undefined;
         this.bounds = convertedSource.bounds; //?: [number, number, number, number] | undefined;
         this.scheme = convertedSource.scheme; //?: "xyz" | "tms" | undefined;
         this.minzoom = convertedSource.minzoom; //?: number | undefined;
         this.maxzoom = convertedSource.maxzoom; //?: number | undefined;
-        this.promoteId = convertedSource.promoteId; //?: PromoteIdSpecification | undefined;
         super.load();
       });
     }
 
     async convertToSource() {
-      let cartoMapConfig = {
-        version: '1.3.0',
-        layers: [{
-          'type': 'mapnik',
-          'options': {
-            'sql': this._originalSource.sql,
-            'srid': '3857'
-          }
-        }]
-      } as CartoApiV1CreateMap;
 
-      if (this._originalSource.minzoom !== undefined) cartoMapConfig.minzoom = this._originalSource.minzoom.toString();
-      if (this._originalSource.maxzoom !== undefined) cartoMapConfig.maxzoom = this._originalSource.maxzoom.toString();
-      //if (this.source.bounds) {
-      // TODO maplibre only support 4326 and carto wants 3857
-      //cartoMapConfig.extent = JSON.stringify(this.source.bounds);
-      //}
+      const url = new URL(`${this._originalSource.url}`);
 
-      let url = new URL(`https://${this._originalSource.server}/user/${this._originalSource.user}/api/v1/map`);
+      url.searchParams.append('request', 'GetCapabilities');
+      url.searchParams.append('service', 'WMS');
 
-      // carto.com uses a different format than on-prem
-      if (this._originalSource.server === 'carto.com') {
-        url = new URL(`https://${this._originalSource.user}.${this._originalSource.server}/api/v1/map`);
-      }
-
-      url.searchParams.append('config', JSON.stringify(cartoMapConfig));
-
-      let tileUrls = [] as Array<string>;
+      let wmsCapabilities: Document;
       try {
-        const resp = await fetch(url);
-        const json = await resp.json();
-        tileUrls = json.metadata.tilejson.vector.tiles as Array<string>;
+        const response = await fetch(url);
+        const text = await response.text();
+        wmsCapabilities = new window.DOMParser().parseFromString(text, "text/xml");
       } catch (e) {
-        throw new Error(`${this._originalSource.type} source failed to load`);
+        throw new Error('Cannot get WMS Capabilities');
+      }
+      const imageFormats = [...(wmsCapabilities.getElementsByTagName('GetMap')[0].children)]
+        .filter(c => c.tagName === 'Format')
+        .map(c => c.textContent);
+      const wmsVersion = wmsCapabilities.getElementsByTagName('WMS_Capabilities')[0].getAttribute('version');
+
+      console.log('Image Formats', imageFormats, 'version', wmsVersion);
+      (window as any).cap = wmsCapabilities;
+
+      const wmsQuery: Omit<WmsApiLayerOptions, 'version' | 'srs' | 'bbox'> = {
+        'layers': this._originalSource.layers.join(','),
+        'format': 'png32', // TODO check with version
+        'height': this._originalSource.tileSize?.toString() as string,
+        'width': this._originalSource.tileSize?.toString() as string,
+        'service': 'WMS',
+        'request': 'GetMap',
+        'transparent':  this._originalSource.transparent as boolean,
+        'styles': '' // TODO
+      };
+
+      if (wmsVersion === '1.3.0') {
+        (wmsQuery as WmsApiLayerOptions130).version = '1.3.0';
+        (wmsQuery as WmsApiLayerOptions130).crs = 'EPSG:3857';
+      } else {
+        (wmsQuery as WmsApiLayerOptions).version = '1.1.1';
+        (wmsQuery as WmsApiLayerOptions).srs = 'EPSG:3857';
       }
 
-      let maplibrarySource: VectorSourceSpecification = {
+      const tileUrl = new URL(`${this._originalSource.url}`);
+      Object.keys(wmsQuery).map(key => {
+        tileUrl.searchParams.append(key, (wmsQuery as any)[key])
+      });
+
+      let maplibrarySource: RasterSourceSpecification = {
         ...this._originalSource,
         ...{
-          'type': 'vector',
-          'tiles': tileUrls
+          'type': 'raster',
+          'tiles': [tileUrl.toString() + '&bbox={bbox-epsg-3857}']
         }
       };
-      return maplibrarySource as Required<VectorSourceSpecification>;
+      return maplibrarySource as Required<RasterSourceSpecification>;
     }
   };
 };
