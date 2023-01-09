a42cc0122743e432fa7ee083dc699d79b6767069
Jim McAndrew <jim@loc8.us>
2023-01-09 14:07:29 -0700

    Remove extra files

--- dist/src/defaultMultiPopup.d.ts
+++
@@ -1,4 +0,0 @@
-import { Map as maplibreMap, Popup } from 'maplibre-gl';
-import { QueryFeature, popupTemplate } from '.';
-import BindPopup from './bindpopup';
-export default function formatter(features: Array<QueryFeature>, _: popupTemplate, map?: maplibreMap, activePopups?: Map<'tooltip' | 'popup', Map<string, BindPopup>>, parentPopup?: Popup): HTMLSpanElement;
