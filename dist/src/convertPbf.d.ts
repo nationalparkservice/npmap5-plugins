f39fc07b12ee956f6a623b201ce9832ec108be8e
Jim McAndrew <jim@loc8.us>
2023-01-09 14:02:35 -0700

    Remove extra dist files

--- dist/src/convertPbf.d.ts
+++
@@ -1,7 +0,0 @@
-import { esriFeatureServiceResponse, esriPbfFeatureResult } from './types/arcgisRestTypes';
-export declare class ConvertPbf {
-    data: ArrayBuffer;
-    constructor(pbfData: ArrayBuffer);
-    convert(): Promise<esriFeatureServiceResponse>;
-    _buildResponse(featureResult: Partial<esriPbfFeatureResult>, features: esriFeatureServiceResponse['features']): esriFeatureServiceResponse;
-}
